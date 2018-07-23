acc87fc9c47f481801bc83627afc1cfc9c0ab43e
sfallmann <jorundr@hotmail.com>
2018-07-23 06:54:41 -0400

    Fixed typo - changed port to PORT

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get("/api/whoami", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.port, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
